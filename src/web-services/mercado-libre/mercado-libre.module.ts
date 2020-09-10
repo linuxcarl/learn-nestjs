930c5ef9bb7a7a7116dff43d104982c25b5d862a
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-09 20:33:33 -0500

    added service mercadoLibre

--- src/web-services/mercado-libre/mercado-libre.module.ts
+++ src/web-services/mercado-libre/mercado-libre.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MercadoLibreController } from './controllers/mercado-libre/mercado-libre.controller';
+import { MercadoLibreService } from './services/mercado-libre/mercado-libre.service';
 
 @Module({
-  controllers: [MercadoLibreController]
+  controllers: [MercadoLibreController],
+  providers: [MercadoLibreService]
 })
 export class MercadoLibreModule {}
